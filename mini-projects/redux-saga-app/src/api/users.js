dee3538592a00093d533a057ee3988062f4f8931
HristijanStrkovski <hristijan-13@live.com>
2019-01-02 23:50:43 +0100

    redux-saga-app beggining

---
+++ mini-projects/redux-saga-app/src/api/users.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const getUsers = () => {
+    axios.get('/users', {
+        params: {
+            limit: 1000
+        }
+    })
+}
---
+++ mini-projects/redux-saga-app/src/sagas/users.js
@@ -0,0 +1,26 @@
+import { takeEvery, call, fork } from 'redux-saga/effects';
+import * as actions from '../actions/users';
+import * as api from '../api/users';
+
+
+function* getUsers() {
+    try {
+        const result = yield call(api.getUsers);
+
+        console.log(result);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+function* watchGetUsersRequest() {
+    yield takeEvery(actions.Types.GET_USERS_REQUEST, getUsers);
+}
+
+const usersSagas = [
+    fork(watchGetUsersRequest)
+];
+
+export default usersSagas;
